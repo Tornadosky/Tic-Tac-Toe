ece5ba75fbb8fd8e0a301680ceb3f61c34852ba1
Ivan Smirnov <ivan.smirnov@stud.th-deg.de>
2022-08-20 14:51:42 +0300

    add random and unbeatable AI

--- tic_tac_toe_MVC.py
+++ tic_tac_toe_MVC.py
@@ -1,6 +1,9 @@
 import pygame
 import numpy
 import random
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 # specify constants to avoid magic numbers
 WIDTH, HEIGHT = (420, 420)
@@ -21,6 +24,74 @@ pygame.display.set_caption("Tic Tac Toe")
 # establish width and height of window and add color
 surface = pygame.display.set_mode((WIDTH, HEIGHT))
 surface.fill(GREY)
+
+
+class AI:
+    # level 0 - random ai
+    # level 1 - unbeatable ai
+    def __init__(self, level=0, player_num=2):
+        self.level = level
+        self.player_num = player_num
+
+    # choose random empty square
+    @staticmethod
+    def choose_random(board):
+        empty_squares = board.get_empty_squares()
+        i = random.randint(0, len(empty_squares) - 1)
+        return empty_squares[i]
+
+    # minimax to choose best moves
+    def minimax(self, board, maximizing):
+        # ai wins
+        if board.is_winner(self.player_num):
+            return 1
+
+        # player wins
+        if board.is_winner(self.player_num ^ 3):
+            return -1
+
+        # draw
+        if board.is_board_full():
+            return 0
+
+        if maximizing:
+            best_score = -1000
+            empty_squares = board.get_empty_squares()
+            for (row, col) in empty_squares:
+                board.change_square(row, col, self.player_num)
+                score = self.minimax(board, False)
+                board.undo(row, col)
+                if score > best_score:
+                    best_score = score
+            return best_score
+        else:
+            best_score = 1000
+            empty_squares = board.get_empty_squares()
+            for (row, col) in empty_squares:
+                board.change_square(row, col, self.player_num ^ 3)
+                score = self.minimax(board, True)
+                board.undo(row, col)
+                if score < best_score:
+                    best_score = score
+            return best_score
+
+    def find_best_move(self, board):
+        best_score = -1000
+        for (row, col) in board.get_empty_squares():
+            board.change_square(row, col, self.player_num)
+            score = self.minimax(board, False)
+            board.undo(row, col)
+            if score > best_score:
+                best_score = score
+                row_best, col_best = (row, col)
+        return row_best, col_best
+
+    # decide how to act depending on level
+    def choose_pos(self, board):
+        if self.level == 0:
+            return self.choose_random(board)
+        else:
+            return self.find_best_move(board)
 
 
 class Model:
@@ -121,11 +192,29 @@ class Controller:
     def __init__(self):
         self.board = Model()
         self.view = View()
+        self.ai = AI()
+        self.mode = 0  # 0 - vs Player, 1 - vs AI
         self.player_num = 1
 
     def restart(self):
         self.view.draw_board()
         self.__init__()
+        logging.debug("PVP on")
+
+    def change_mode(self):
+        self.mode = self.mode ^ 1
+        if self.mode == 0:
+            self.change_ai(0)
+            logging.debug("PVP on")
+        else:
+            logging.debug("AI on")
+
+    def change_ai(self, level):
+        self.ai.level = level
+        if level == 1:
+            logging.debug("unbeatable AI on")
+        else:
+            logging.debug("random AI on")
 
     def is_over(self):
         return self.board.is_winner(1) or self.board.is_winner(2) or self.board.is_board_full()
@@ -138,6 +227,7 @@ class Controller:
 def main():
     game = Controller()
     game.restart()
+    ai = game.ai
     board = game.board
 
     running = True
@@ -145,10 +235,20 @@ def main():
         for event in pygame.event.get():
             # event handler for pressing keys
             if event.type == pygame.KEYDOWN:
+                # change the game mode if 'c' is pressed
+                if event.key == pygame.K_c:
+                    game.change_mode()
                 # restart the game if 'r' is pressed
                 if event.key == pygame.K_r:
                     game.restart()
+                    ai = game.ai
                     board = game.board
+                # change to random AI
+                if event.key == pygame.K_0:
+                    game.change_ai(0)
+                # change to unbeatable AI
+                if event.key == pygame.K_1:
+                    game.change_ai(1)
             # click event
             if not game.is_over() and event.type == pygame.MOUSEBUTTONDOWN:
                 # get coordinates of mouse click
@@ -162,6 +262,15 @@ def main():
                     game.view.draw_figures(board)
                     if game.is_over():
                         game.view.draw_winning_lines(board)
+            # AI making move event
+            if not game.is_over() and game.player_num == ai.player_num and game.mode == 1:
+                pygame.display.update()
+                row, col = ai.choose_pos(board)
+
+                game.make_move(row, col)
+                game.view.draw_figures(board)
+                if game.is_over():
+                    game.view.draw_winning_lines(board)
 
             # event handler to handle quitting the application
             if event.type == pygame.QUIT:
